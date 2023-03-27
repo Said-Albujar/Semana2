8be3d6384b40becc82d82089da25a48eb7d831be
Said Albujar Torres <saidalbujar23@gmail.com>
2023-03-27 18:28:57 -0500

    Figura

---
+++ Ejercicios/Ejercicios/Figura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class Figura
+    {
+        public virtual float CalculateArea()
+        {
+            return 0;
+        }
+    }
+}
