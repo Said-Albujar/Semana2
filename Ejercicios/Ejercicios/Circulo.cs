3311833db12db298e48a156cdee84b86a1f3286f
Said Albujar Torres <saidalbujar23@gmail.com>
2023-03-27 19:28:23 -0500

    Circulo

---
+++ Ejercicios/Ejercicios/Circulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios
+{
+    class Circulo : Figura
+    {
+        private float radius;
+
+        public Circulo(float radius)
+        {
+            this.radius = radius;
+        }
+
+        public override float CalculateArea()
+        {
+            return radius * radius * 3.14f;
+        }
+    }
+}
